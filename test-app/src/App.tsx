42e45035d67d36cbe2f46767c42ee447ea53576c
Ajinkya-WSP <Ajinkya.Bhosale@wsp.com>
2023-05-08 14:18:52 -0400

    Updated the access token

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
         <div className='viewer-window'>
             <Viewer
-              runtime={{ accessToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlU3c0dGRldUTzlBekNhSzBqZURRM2dQZXBURVdWN2VhIiwicGkuYXRtIjoiN3ozaCJ9.eyJzY29wZSI6WyJ2aWV3YWJsZXM6cmVhZCJdLCJjbGllbnRfaWQiOiJFa2dpRTRiSlZYcnp6QWVBdzliR2N3UDB3T1VVa3VHViIsImF1ZCI6Imh0dHBzOi8vYXV0b2Rlc2suY29tL2F1ZC9hand0ZXhwNjAiLCJqdGkiOiJ1Q3BneXpqdDh6MW5VaU51Sk5WZVBnYUxuZ3lObmtJdGQ2TjZ0bDZhRGJ4MmtwZG0zZHpmOEdlVTFlMkdvbkNPIiwiZXhwIjoxNjgzNTYwOTA4fQ.O5jdER_NebuWMU944JKDMh5nvMPQS6oF9Yx-SbelhfrBZ3JYdplhZ4wxvidr7bBYdZBjYTdZnVitjyCy8Z1XPqZQii5hWduUdb0gnzbFJptmJhHIn04t1hhGv5oolvqoNvQdU1bhfkfMXMEX9J693Uzab_ai6fD0jxJN1CvnJOmdLe81TibYEkfLZYALYRmOFs4lbAWKJOdFNC6VqulJJGaWjpAb7nYBXnsXFfqCaDY-_RC_8pkNDqa5gnmKWVo2WMXH0G0PRJvh2gCZt1ccSBC0RIOKBN-neHqvnF3HzyS3-awcBYx0PXi_XUhJu1SYpNhAY50flKk81sNOKaEUyw' }}
+              runtime={{ accessToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlU3c0dGRldUTzlBekNhSzBqZURRM2dQZXBURVdWN2VhIiwicGkuYXRtIjoiN3ozaCJ9.eyJzY29wZSI6WyJ2aWV3YWJsZXM6cmVhZCJdLCJjbGllbnRfaWQiOiJFa2dpRTRiSlZYcnp6QWVBdzliR2N3UDB3T1VVa3VHViIsImF1ZCI6Imh0dHBzOi8vYXV0b2Rlc2suY29tL2F1ZC9hand0ZXhwNjAiLCJqdGkiOiJObmQ3blNxRk9UR2dBMU8zV2FEY0tPVWp4VjhtcUM1TVlVd3VQaGg1bzFDQ2JPNWQ5ekdRZ0RpNzFNb1AwY2d5IiwiZXhwIjoxNjgzNTczMzg3fQ.BbcawSMwgdrzOgItoXnBD2SwOlvglSJsaEdp2J3ejoXPvf5zGlP_2ERRixCkVB2v-_FY5wrq9eoesw_wYxCXjTU2N1TOxkp7I9UkkQqKR5M5MEVcacEHB7C3f70wBs-HLP8LszuO-aBVGp8v-tNfaTzoQ62-Fzog4uFqCzabWP63dmjTJ3VwmVRLjIntWEiMSQ-HEtbf_OEhIBmy3yS-vZgak2nLNf0Aq-tf14mwDwOBhH-ekPbexP5Re_kdj-7HtPfyl0AU7u20mQCFGKAwotsX3YBUqFkcNeUGUfwqG_8c5UhTgGbdCmXsohaHSUM5YGu6pSyxwH8ULAlcqgDCpA' }}
               urn={'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Zm9yZ2UtY29kZXBlbi1tb2RlbHMvcmFjLWFkdmFuY2VkLXNhbXBsZS1wcm9qZWN0LnJ2dA'}
             />
         </div>
@@ -16,5 +16,5 @@ function App() {
     </div>
   );
 }
-
+ 
 export default App;
